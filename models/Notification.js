95d041abb133f574b118d3a2920d40814135dc55
TimJLee <tim0409@hotmail.com>
2020-03-11 11:49:34 +0900

    Remark: :speech_balloon: add annotation for schema

--- models/Notification.js
+++ models/Notification.js
@@ -12,3 +12,14 @@ var notificationSchema = mongoose.Schema({
 
 var Notification = mongoose.model('notification', notificationSchema);
 module.exports = Notification;
+/*
+DB Schema
+NO	컬럼ID		컬럼명		           type		  null		key 
+1	id		      알림번호  	         string		n.n	  	p.k
+2	send_user	  알림보낸사람id	     string	  n.n
+3	rec_user		알림받는사람id	     string		n.n
+4	contents		알림내용	           string		n.n
+5	createdAt	  알림 생성시각	       Date	  	n.n
+6	post_id		  게시물의 object id	string	
+7	status		  알림구분	          string		n.n
+*/
