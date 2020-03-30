af88058d41804ac59a64030daa907ed32cb22394
TimJLee <tim0409@hotmail.com>
2020-03-30 12:57:34 +0900

    Feat: :zap: add socket to app.js

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var { sequelize } = require('./models/User.js');
+var { sequelize } = require('./models/user.js');
+var {sequelize} = require('./models');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var createError = require('http-errors');
 var logger = require('morgan');
 var app = express();
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ port: 9090 });
+app.io = require('socket.io')();
+var client = require('./cache_redis');
+
+
 require('dotenv').config();
 
 // DB setting
-const driver = async () => {
-  try {
-      await sequelize.sync();
-  } catch (err) {
-      console.error('user table sync fail');
-      console.error(err);
-      return;
-  }
-
-  console.log('user table sync complete');
-};
-
-driver();
+sequelize.sync();
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect(process.env.MONGO_DB);
+mongoose.connect('mongodb://localhost:27017/Post');
 var db = mongoose.connection;
 db.once('open', function(){
   console.log('DB connected');
@@ -41,9 +32,10 @@ db.on('error', function(err){
 // Other settings
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -53,21 +45,43 @@ app.use(function (req, res, next) {
 
 // API
 app.use('/noti', require('./routes/notifications'));
+/*
+app.io.on('connection',function(socket){
+  console.log('a user');
+  socket.on('init',function(data){
+    console.log(data.name);
+    socket.emit('welcome',"TEST");
+  });
+})
+*/
+app.io.on('connection', function(socket) {
+  console.log('a user connected');
+  
+	socket.emit('server connection', { server: 'socket on' });
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  socket.on('client connection', function (data) {  
+    console.log(data);
+  });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  socket.on('client log on', function(data) {
+      client.set(data.userId, socket.id, function(err) {
+      if (err) throw err;
+        console.log("socket.id on redis complete with socketid : " + socket.id);
+      });
+    });
+  
+  socket.on('disconnect', function() {
+    client.del(socket.id, function(err, response) {
+      if (response == 1) {
+        console.log("user socket deleted from redis successfully!")
+      } else{
+        console.log("user socket from redis delete fail")
+      }
+    });
+  });
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.locals.io = app.io;
 
 module.exports = app;
 
